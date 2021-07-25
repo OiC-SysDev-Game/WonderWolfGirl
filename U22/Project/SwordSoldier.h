23d1c48d29d7de2dff5047fd964c9ad5afb14f22
FOE1003 <towa.k100@icloud.com>
2021-07-25 15:17:01 +0900

    キャラクター関連の処理追加

---
+++ U22/Project/SwordSoldier.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "U22.h"
+#include "Enemy.h"
+
+class CSwordSoldier : public CEnemy
+{
+private:
+	bool isMove = false;
+	float height = 240;
+	float width = 130;
+
+	float search_Range = 300;
+	float invicible_Time = 1;
+	float attack_Rate = 3;
+
+public:
+	bool Load();
+	void Initialize();
+	void Update();
+	void Render(CCamera* _camera);
+	void RenderDebug(CCamera* _camera);
+	void Release();
+
+	void Acceleration(void);
+	void Neutral(void);
+	void Move(void);
+	void Attack(void);
+
+};
